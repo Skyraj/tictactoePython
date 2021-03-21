ee3ef48a52c0a97dd8baeae9d5600650db0f3192
Jack Skiera <skyrajang@gmail.com>
2021-03-21 18:44:37 -0400

    basic functionality completed

--- Tic Tac Toe/tictactoe.py
+++ Tic Tac Toe/tictactoe.py
@@ -1,7 +1,6 @@
 board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 
 def drawBoard():
-    print("Let's Play Tic Tac Toe!\n")
     print(" " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " ")
     print("---+---+---")
     print(" " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " ")
@@ -9,21 +8,21 @@ def drawBoard():
     print(" " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " \n")
 
 def checkVictory():
-    if board[0][0] == board[0][1] == board[0][2]:
+    if all(s == 'X' for s in board[0]) or all(s == 'O' for s in board[0]):
         return True
-    elif board[1][0] == board[1][1] == board[1][2]:
+    elif all(s == 'X' for s in board[1]) or all(s == 'O' for s in board[1]):
         return True
-    elif board[2][0] == board[2][1] == board[2][2]:
+    elif all(s == 'X' for s in board[2]) or all(s == 'O' for s in board[2]):
         return True
-    elif board[0][0] == board[1][0] == board[2][0]:
+    elif board[0][0] == board[1][0] == board[2][0] == 'X' or board[0][0] == board[1][0] == board[2][0] == 'O':
         return True
-    elif board[0][1] == board[1][1] == board[2][1]:
+    elif board[0][1] == board[1][1] == board[2][1] == 'X' or board[0][1] == board[1][1] == board[2][1] == 'O':
         return True
-    elif board[0][2] == board[1][2] == board[2][2]:
+    elif board[0][2] == board[1][2] == board[2][2] == 'X' or board[0][2] == board[1][2] == board[2][2] == 'O':
         return True
-    elif board[0][0] == board[1][1] == board[2][2]:
+    elif board[0][0] == board[1][1] == board[2][2] == 'X' or board[0][0] == board[1][1] == board[2][2] == 'O':
         return True
-    elif board[0][2] == board[1][1] == board[2][0]:
+    elif board[0][2] == board[1][1] == board[2][0] == 'X' or board[0][2] == board[1][1] == board[2][0] == 'O':
         return True
     else:
         return False
@@ -35,12 +34,14 @@ def ttt():
 
     while (turn_count <= 9):
         x, y = input("Input Position: ").split()
+        x, y = int(x), int(y)
         if board[x][y] == ' ':
             board[x][y] = turn
         # Need to elaborate on error case to ask for input again
         else:
             print("Position taken!")
         
+        drawBoard()
         if checkVictory():
             break
         
@@ -50,12 +51,12 @@ def ttt():
             turn = 'X'
 
         turn_count += 1
-        drawBoard()
     
     if turn_count % 2 == 0:
         print("Player X Wins!")
     else:
         print("Player O Wins!")
 
+print("Let's Play Tic Tac Toe!\n")
 drawBoard()
 ttt()
