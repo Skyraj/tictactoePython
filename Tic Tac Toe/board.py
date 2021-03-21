ead77a8161002539e32c24faaa07adb75dd34d2d
Jack Skiera <skyrajang@gmail.com>
2021-03-21 02:24:51 -0400

    removed for simplicity

--- Tic Tac Toe/board.py
+++
@@ -1,9 +0,0 @@
-class Board:
-    def __init__(self):
-        self.grid = [['','',''],['','',''],['','','']]
-    
-    def check(self):
-        if self.grid[0][0] != '' and self.grid[0][0] == self.grid[1][1] and self.grid[1][1] == self.grid[2][2]:
-            pass
-        elif self.grid[0][0] != '' and self.grid[0][0] == self.grid[1][1] and self.grid[1][1] == self.grid[2][2]:
-            pass
--- Tic Tac Toe/player.py
+++
@@ -1,7 +0,0 @@
-class Player:
-    def __init__(self, symbol):
-        self.symbol = symbol
-    
-    def placeSymbol(self, x, y):
-        pass
-
