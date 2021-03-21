fd9b7a252753e291b6ee44ec20c4a11cef56b370
Jack Skiera <skyrajang@gmail.com>
2021-03-21 03:03:50 -0400

    updated drawBoard

--- Tic Tac Toe/tictactoe.py
+++ Tic Tac Toe/tictactoe.py
@@ -1,7 +1,14 @@
-ttt_board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 
 def drawBoard():
-    pass
+    print("Let's Play Tic Tac Toe!")
+    print(" " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " ")
+    print("---+---+---")
+    print(" " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " ")
+    print("---+---+---")
+    print(" " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " ")
 
 def ttt():
     pass
+
+drawBoard()
